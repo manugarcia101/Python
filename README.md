98e6d54033ea5f1d0db581ada19d984c4ac5d412
Manuel Garcia Lopez <mgl00050@red.ujaen.es>
2018-06-02 12:28:30 +0200

    "Fixing errors"

--- README.md
+++
@@ -1,2 +0,0 @@
-# Python
-This repository will contain all the little activities and my personal progress in Python language
--- prueba.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-#! python3
-
-import sys
-import time
-print(sys.version)
-time.sleep(7)
-print(sys.version) 
