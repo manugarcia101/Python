609e617b15d07c224be7e8154c9b9a886a616e30
Manuel Garcia Lopez <manugarcial96@gmail.com>
2018-06-03 13:37:08 +0200

    Math operations and strings concatenations
    All using print function

--- test/testInit.py
+++ test/testInit.py
@@ -1,4 +1,29 @@
 #! Python 3.6.3
 #Author: Manuel Garcia Lopez
 
-print("hello world")
+#This is a single line comment
+
+'''
+This is a multiple line comment
+'''
+
+#Some print lines examples
+print("I am printing code")
+print("We're going to the store")
+print("Number: ", 5)
+
+#Strings concatenations
+print("Hi "+"there")
+print("Number " + str(5)) #conversion int to string needed
+
+#other strange things we can do
+print(int('8')+5)
+print(float('8.8')+5)
+
+#mathematics operations with python
+# + - * /
+print(1+3)
+print(1-3)
+print(2*3)
+print(5/2)
+print(4**4)#exponential multiplications
