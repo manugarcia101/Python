48df0a040dfa1ed97761789c6b52819f43c79d0f
Manuel Garcia Lopez <manugarcial96@gmail.com>
2018-06-22 19:27:17 +0200

    Matplot lib intro

---
+++ Python (Data Analisis)/MatplotlibIntro.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 22 10:46:04 2018
+
+@author: Manu
+"""
+
+#! Python 3.6.5
+#Author: Manuel Garcia Lopez
+
+#Matplotlib intro
+
+from matplotlib import pyplot as plt 
+
+plt.plot([5,6,7,8],[7,3,8,3])
+
+plt.show()
