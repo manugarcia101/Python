7c2f43bc1097f372f54bef2d6a3c447356cc8f9e
Manuel Garcia Lopez <manugarcial96@gmail.com>
2018-07-03 19:33:48 +0200

    adding and using templates with flask

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -1,6 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+post = [
+    {
+        'author': 'Manu Garcia',
+        'title': 'First python web',
+        'content': 'First content',
+        'date_posted': 'July 03, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog',
+        'content': 'Second content',
+        'date_posted': 'July 04, 2018'
+    }
+]
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts = post)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title = 'About us')
+
+''' To run the script directly, I setted this function
+    ONLY FOR THE DEVELOPMENT PROCESS'''
+if __name__ == '__main__':
+    app.run(debug=True)
