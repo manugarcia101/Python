7ad6d5ab3701f18dc79c44a73812eb34b77fb236
Manuel Garcia Lopez <manugarcial96@gmail.com>
2018-07-13 21:20:43 +0200

    Reset Password

--- Flask_app/app/__init__.py
+++ Flask_app/app/__init__.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt 
+from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5891628bb0b13ce0c676dfde280ba245'
@@ -10,9 +11,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login' 
-#route where it will be redirected
+login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-#to improve the alert access
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('PASS')
+mail = Mail(app)
 
 from app import routes
