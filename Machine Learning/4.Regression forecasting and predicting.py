e330e8aae1d1a4d0ecd1b91cf45fd382e6c45b79
Manuel Garcia Lopez <manugarcial96@gmail.com>
2018-07-20 19:52:59 +0200

    forecasting and predicting

--- Machine Learning/4.Regression forecasting and predicting.py
+++ Machine Learning/4.Regression forecasting and predicting.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul 17 18:40:46 2018
+Created on Fri Jul 20 18:40:26 2018
 
 @author: Manu
 """
@@ -10,10 +10,14 @@ Created on Tue Jul 17 18:40:46 2018
 
 #Machine Learning
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -30,14 +34,15 @@ df.fillna(-99999, inplace=True)
 forecast_out = int(math.ceil(0.01*len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
+
 
 x = np.array(df.drop(['label'],1))
-y = np.array(df['label'])
-
+x = x[:-forecast_out]
+x_lately = x[-forecast_out:]
 x = preprocessing.scale(x)
 
-#x = x[:-forecast_out+1] 
+df.dropna(inplace=True)
+y = np.array(df['label'])
 y = np.array(df['label'])
 
 x_train, x_test, y_train, y_test = cross_validation.train_test_split(x, y, test_size = 0.2)
@@ -47,5 +52,25 @@ clf = LinearRegression(n_jobs = -1)
 #clf=svm.SVR(kernel = 'poly') #support vector regression algorithm
 clf.fit(x_train, y_train)
 accuracy = clf.score(x_test, y_test) 
+#print(accuracy)
 
-print(accuracy)
+forecast_set = clf.predict(x_lately)
+print(forecast_set, accuracy, forecast_out)
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set: 
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+    
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
